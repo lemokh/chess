716adc71f56de4c300c3e4da54539286130ef87b
lemokh <byndasma@gmail.com>
2017-11-22 20:30:43 +0000

    pieces.js

---
+++ main/js/pieces.js
@@ -0,0 +1,43 @@
+const pieces = [
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    { name: 'pawn', x: , y: },
+    
+    { name: 'knight', x: , y: },
+    { name: 'knight', x: , y: },
+    
+    { name: 'knight', x: , y: },
+    { name: 'knight', x: , y: },
+    
+    { name: 'bishop', x: , y: },
+    { name: 'bishop', x: , y: },
+    
+    { name: 'bishop', x: , y: },
+    { name: 'bishop', x: , y: },
+    
+    { name: 'rook', x: , y: },
+    { name: 'rook', x: , y: },
+    
+    { name: 'rook', x: , y: },
+    { name: 'rook', x: , y: },
+    
+    { name: 'queen', x: , y: },
+    { name: 'queen', x: , y: },
+    
+    { name: 'king', x: , y: },
+    { name: 'king', x: , y: },
+];
