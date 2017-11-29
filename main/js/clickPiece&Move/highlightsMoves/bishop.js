3b219ca7cbee9df7136cbd7383851cb07d530f64
lemokh <byndasma@gmail.com>
2017-11-29 23:32:48 +0000

    bishop

--- main/js/clickPiece&Move/highlightsMoves/bishop.js
+++ main/js/clickPiece&Move/highlightsMoves/bishop.js
@@ -1,6 +1,6 @@
 // write a function that on-click of bishop square
 // limits where bishop can be dropped
-// ('div').click(function() {});
+// ('div').click(function() {switch(id) { 64 cases } });
 
 function bishopClick(square) {
 
