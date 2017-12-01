083d81932a4ec76bcb08c853214ab31c5ac065df
lemokh <byndasma@gmail.com>
2017-12-01 23:54:07 +0000

    pieces.js

--- main/js/pieces.js
+++ main/js/pieces.js
@@ -1,65 +1,61 @@
-function init() {
-
-    function insert(arr) {
-        arr.forEach((item) => {
-            const id = item.x + item.y;
-            const img = document.createElement("img");
-            img.src = item.image;
-            document.getElementById(id).appendChild(img);
-        });
-    }
-
-    let blacks = [
-        { name: 'pawn', x: "0", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "1", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "2", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "3", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "4", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "5", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "6", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "7", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'knight', x: "1", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'knight', x: "6", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'bishop', x: "2", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'bishop', x: "5", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'rook', x: "0", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'rook', x: "7", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'queen', x: "3", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'king', x: "4", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-    ];
-
-    let whites = [
-        { name: 'pawn', x: "0", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "1", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "2", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "3", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "4", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "5", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "6", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'pawn', x: "7", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'knight', x: "1", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'knight', x: "6", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'bishop', x: "2", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'bishop', x: "5", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'rook', x: "0", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-        { name: 'rook', x: "7", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'queen', x: "3", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
-
-        { name: 'king', x: "4", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' }
-    ];
-
-    let pieces = [blacks, whites].reduce((b, w) => b.concat(w), []);
-
-    // console.log(pieces);
-    insert(pieces);
-
+function insert(arr) {
+    arr.forEach((item) => {
+        const id = item.x + item.y;
+        const img = document.createElement("img");
+        img.src = item.image;
+        document.getElementById(id).appendChild(img);
+    });
 }
+
+let blacks = [
+    { name: 'pawn', x: "0", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "1", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "2", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "3", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "4", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "5", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "6", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "7", y: "1", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'knight', x: "1", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'knight', x: "6", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'bishop', x: "2", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'bishop', x: "5", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'rook', x: "0", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'rook', x: "7", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'queen', x: "3", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'king', x: "4", y: "0", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+];
+
+let whites = [
+    { name: 'pawn', x: "0", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "1", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "2", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "3", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "4", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "5", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "6", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'pawn', x: "7", y: "6", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'knight', x: "1", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'knight', x: "6", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'bishop', x: "2", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'bishop', x: "5", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'rook', x: "0", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+    { name: 'rook', x: "7", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'queen', x: "3", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' },
+
+    { name: 'king', x: "4", y: "7", image: 'https://github.com/lemokh/Photos/blob/master/whitePawn.png?raw=true' }
+];
+
+let pieces = [blacks, whites].reduce((b, w) => b.concat(w), []);
+
+// console.log(pieces);
+insert(pieces);
