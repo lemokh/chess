39d672fb4d379d4b3fcac91aed52707ca13bedad
lemokh <byndasma@gmail.com>
2018-03-11 19:29:37 +0000

    update

--- main/js/click&Move/isCheck.js
+++ main/js/click&Move/isCheck.js
@@ -1,5 +1,1 @@
-function isCheck(pieces, player) { // returns either array of checking pieces or false
-  setSides(pieces);
-  if (player === 0) { inCheck(whiteKing); }
-  else { inCheck(blackKing); }
-}
+
