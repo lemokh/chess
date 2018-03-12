9911852cb4bbeba484c8810b3523afd38bb33592
lemokh <byndasma@gmail.com>
2018-03-12 20:22:12 +0000

    update

--- main/js/click&Move/isMate.js
+++ main/js/click&Move/isMate.js
@@ -1,4 +1,1 @@
-function isMate(pieces, player) { // returns true/false if king checkmated
-  if (player === 0) { return kingFree(whiteKing, whites); }
-  else { return kingFree(blackKing, blacks); }
-}
+
