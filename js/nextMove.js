a5a4cae4e3118c24daad3ab38ec48e45316ebd0f
byndasma <byndasma@gmail.com>
2018-03-29 19:20:06 -0700

    nextMove.js

--- js/nextMove.js
+++ js/nextMove.js
@@ -1,17 +1,22 @@
 function nextMove() {
-    renderBoard();
-    checkMate();
-    // add click listener to activeSide
-        // on-click: highlight possible moves
+    renderBoard(); // DONE
+    checkMate(); // FIX --> .side = __
+    // add click listener to activeSide --> DONE
+        // on-click: highlight possible moves --> DONE
     
-    // add click listener to lit spaces
-        // on-click: unhighlight possible moves
+    // add click listener to lit spaces --> DONE
+        // on-click: unhighlight possible moves --> DONE?
+            
             // remove both click listeners --> use .getElementsByClassName()
+            
             // if ( {x2, y2} contains passiveSide --> image div) {
                 // remove that piece from passiveSide array
                 // add that piece to its takenBox div
             // }
+            
             // set moved piece's {x1, x2} to be {x2, y2} within activeSide array
+            
             // update pieces array
+            
             // tempSide = activeSide; activeSide = passiveSide; passiveSide = tempSide;
 }
