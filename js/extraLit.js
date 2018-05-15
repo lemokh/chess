1ca2eaa9f99c1a05cc9228f7bd588eb8c5939a79
byndasma <byndasma@gmail.com>
2018-05-15 11:29:05 -0700

    update extraLit.js

--- js/extraLit.js
+++ js/extraLit.js
@@ -823,18 +823,19 @@ function lit(activeSide, passiveSide) {
     // sets activeKing
     for (i = 0; i < activeSide.length; i++) {
         if (activeSide[i].getAttribute('data-name') === 'king') {
-            activeKing = activeSide[i];
-            break; // --------------------------------------------
-        } console.log('activeKing -->');  console.log(activeKing);
-    } // ---------------------------------------------------------
+            activeKing = activeSide[i];  break;
+        }
+    }  console.log('activeKing -->');  console.log(activeKing);
+    // --------------------------------------------------------
     turn = activeKing.getAttribute('data-side');
     // --------------------------------------------------------------------
     // populates kingAttackers with any passivePiece that checks activeKing
     passiveSide.forEach(passivePiece => {
         if (checkingSpace(passivePiece, activeKing.id)) {
             kingAttackers.push(passivePiece);
-        } console.log('kingAttackers -->');  console.log(kingAttackers);
-    }); // -------------------------------------------------------------
+        }
+    });  console.log('kingAttackers -->');  console.log(kingAttackers);
+    // ----------------------------------------------------------------
     // if activeKing in check
     if (kingAttackers.length) { // via checkingSpace(passivePiece, activeKing.id)
         // console.log();
