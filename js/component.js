148af6a3b97126da59ca2d0d165762a0b4b5668e
byndasma <byndasma@gmail.com>
2018-06-03 11:50:21 -0700

    component.js

--- js/component.js
+++ js/component.js
@@ -1,23 +1,35 @@
+/*  for each pinnedPiece,
+        collect into pinnedPiece's pinnedMoveIds attribute:
+            ids between it & its pinner
+            then also ids between it & its own king */
+
 pinnedPieces.forEach(pinnedPiece => {
     // --------------------------------------------------------------------
     checkingSpace(pinnedPiece, pinnedPiece.getAttribute('data-pinner').id);
-    possiblePinnedMoves = pathOfCheck;
-    // ---------------------------------------
+    pinnedPiece.setAttribute('data-pinnedMoves', pathOfCheck);
+    // -------------------------------------------------------
     checkingSpace(pinnedPiece, activeKing.id);
-    possiblePinnedMoves = [...pathOfCheck];
+    pinnedPiece.setAttribute('data-pinnedMoves', [...pathOfCheck]);
 });
 
+/*  if pieceToMove is pinned,
+        then for each id in its pinnedMoveIds attribute,
+            if pieceToMove can move to that id,
+                add that id to litDivs array   */
 
 if (pieceToMove.getAttribute('data-pinned') === 'true') {
     // --------------------------------------------------
-    possiblePinnedMoves.forEach(id => {
+    pinnedMoves.forEach(id => {
         // ------------------------------------------------------
         if (checkingSpace(pieceToMove, id)) { litDivs.push(id); }
     });
 }
 
-addLitDivHandler(pinn);
- 
+/*  now that litDivs contains all ids where pinned pieceToMove can go...
+        simply add click-handler to pieceToMove run movePiece(e)    */
+        
+addLitDivHandler(pieceToMove, movePiece);
+
 
 /*
 ERRORS:
