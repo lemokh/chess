e4c97236d48f07f74bcb9855409de39364098b13
byndasma <byndasma@gmail.com>
2018-06-16 14:35:32 -0700

    resign()

--- js/clocks.js
+++ js/clocks.js
@@ -1,146 +1,41 @@
-var interval = null,
-    minuteNum = null,
-    colon = ':',
+var minuteNum,
     tenthSecondStr = '5',
     tenthSecondNum = +tenthSecondStr,
     hundredthSecondStr = '9',
     hundredthSecondNum = +hundredthSecondStr,
-    mainTime = '',
-    focusTime = 10,
-    relaxTime = 10;
 
-function countDown() {
-  interval = setInterval( runClock,  1000 );
-}
+    clock1 = 10,
+    clock2 = 10,
+    
+    runClock1,
+    runClock2,
+    clockToUpdate,
+    clockToRun;
 
-function runClock() { // runs clock down to 0
+function countDown() { // runs clock down to 0
 
-  minute_str = minuteNum.toString();
-
-  tenthSecondStr = tenthSecondNum.toString();
-
-  hundredthSecondStr = hundredthSecondNum.toString();
-
-  mainTime = minute_str + colon + tenthSecondStr + hundredthSecondStr;
-
-  $( 'countDown' ).html( mainTime );
-
+  minuteStr = minuteNum+'';
+  tenthSecondStr = tenthSecondNum+'';
+  hundredthSecondStr = hundredthSecondNum+'';
   hundredthSecondNum -= 1;
 
   if( hundredthSecondNum < 0 ) {
     tenthSecondNum -= 1;
     hundredthSecondNum = 9;
   }
+
   if( tenthSecondNum < 0 ) {
     minuteNum -= 1;
     tenthSecondNum = 5;
   }
-  if( minuteNum < 0 ) {
-    transition();
-  }
+
+  if( minuteNum < 0 ) { endOfGame(); }
+
+  document.getElementById(clockToUpdate).html(
+    minuteStr + ':' + tenthSecondStr + hundredthSecondStr
+  );
 }
 
-function transition() {
-
-  if ( $( 'clocks' ).hasClass( 'focusMode' ) ) { 
-
-    clearInterval( interval );
-    minuteNum = relaxTime;
-    $( 'clocks' ).removeClass( 'focusMode' );
-    $( 'focus, clock1' ).removeClass( 'active' );
-    $( 'relax' ).trigger( 'click' );
-    $( '#relax' ).trigger( 'play' );
-  }
-  else {
-
-    clearInterval( interval );
-    minuteNum = focusTime;
-    $( 'clocks' ).removeClass( 'relaxMode' );
-    $( 'relax, clock2' ).removeClass( 'active' );
-    $( 'focus' ).trigger( 'click' );
-    $( '#focus' ).trigger( 'play' );
-  }
+function runClock(clock) {
+  while (clockToRun) { setInterval( runClock, 1000 ); }
 }
-
-
-$( 'focusTime' ).html( focusTime );
-
-
-$( 'relaxTime' ).html( relaxTime );
-
-
-$( 'focus' ).on( 'click', function runFocus() {
-
-    minuteNum = focusTime - 1;
-    $( 'adjust1, adjust2, focus, relax' ).addClass( 'darken' );
-    $( 'reset' ).removeClass( 'noClick' ).addClass( 'lighten' );
-    $( 'countDown span' ).html( focusTime + ':00' );
-    $( 'focus, clock1' ).addClass( 'active' );
-    $( 'clocks' ).addClass( 'focusMode' );
-    $( 'reset' ).html( 'RESET' );
-
-    countDown();
-} );
-
-
-$( 'relax' ).on( 'click', function runRelax() {
-
-    minuteNum = relaxTime - 1;
-    $( 'adjust1, adjust2, focus, relax' ).addClass( 'darken' );
-    $( 'reset' ).removeClass( 'noClick' ).addClass( 'lighten' );
-    $( 'countDown span' ).html( relaxTime + ':00' );
-    $( 'relax, clock2' ).addClass( 'active' );
-    $( 'clocks' ).addClass( 'relaxMode' );
-    $( 'reset' ).html( 'RESET' );
-    countDown();
-} );
-
-
-$( 'reset' ).on( 'click', function reset() {
-  $( 'clocks' ).removeClass( 'focusMode relaxMode' );
-  location.reload();
-} );
-
-
-$( 'up1' ).on( 'click', function additionFocus() {
-
-  let plus_focus = focusTime + 1;
-
-  if( focusTime < 60 ) {
-    $( 'focusTime' ).html( plus_focus );
-    focusTime = plus_focus;
-  }
-} );
-
-
-$( 'up2' ).on( 'click', function additionRelax () {
-
-  let plus_relax = relaxTime + 1;
-
-  if( relaxTime < 60 ) {
-    $( 'relaxTime' ).html( plus_relax );
-    relaxTime = plus_relax;
-  }
-} );
-
-
-$( 'down1' ).on( 'click', function subtractionFocus () {
-
-  let minus_focus = focusTime - 1;
-
-  if( minus_focus >= 1 ) {
-    $( 'focusTime' ).html( minus_focus );
-    focusTime = minus_focus;
-  }
-} );
-
-
-$( 'down2' ).on( 'click', function subtractionRelax () {
-
-  let minus_relax = relaxTime - 1;
-
-  if ( minus_relax >= 1) {
-    $( 'relaxTime' ).html( minus_relax );
-    relaxTime = minus_relax;
-  }
-} );
--- js/litAF.js
+++ js/litAF.js
@@ -15,125 +15,71 @@ var passiveSide = oranges;
 ///////////////////////////
 ///////////////////////////
 
-// one clock function to run two separate clocks
-// start & stop & alert(activeSide + " resigns"); & resetBtn()
-function clock() {
+var minuteNum = 10,
+    tenthSecondStr = '5',
+    tenthSecondNum = +tenthSecondStr,
+    hundredthSecondStr = '9',
+    hundredthSecondNum = +hundredthSecondStr,
 
-	let seconds, timer, time,
-	  abrv1 = 2500,
-	  abrv2 = 500,
-	  stopped = false;
+    clock1,
+    clock2,
+    
+    runClock1,
+    runClock2,
+	
+	clockToUpdate,
+    clockToRun;
+
+///////////////////////////
+///////////////////////////
+
+function countDown() { // runs clock down to 0
+
+	minuteStr = minuteNum+'';
+	tenthSecondStr = tenthSecondNum+'';
+	hundredthSecondStr = hundredthSecondNum+'';
+	hundredthSecondNum -= 1;
   
-	function convert() {
-	  let temp = "" + abrv1;
-	  if (temp[2] === '0' && temp[3] === '0') {
-		time = temp[0] + temp[1];
-	  }
-	  else if (abrv1 < 1000) { time = temp[0]; }
-	} // converts time
+	if( hundredthSecondNum < 0 ) {
+		tenthSecondNum -= 1;
+	  	hundredthSecondNum = 9;
+	}
   
-	function down() { // runs convert()
-	  if (+abrv1 > 100) { abrv1 -= 100; }
-	  convert();
-	  document.getElementById('time').html(time);
-	  stopped = false;
-	} // decreases timer by one second
+	if( tenthSecondNum < 0 ) {
+	  	minuteNum -= 1;
+	  	tenthSecondNum = 5;
+	}
   
-	function up() { // runs convert()
-	  if (+time < 60) { abrv1 = (+abrv1 + 100); }
-	  convert();
-	  document.getElementById('time1').html(time);
-	  stopped = false;
+	if( minuteNum < 0 ) {
+		resign();
+	}
   
-	} // inceases timer by one second
-  
-	function page() { // runs convert()
-	  convert();
-	  document.getElementById('time1').html(time);
-	}; // displays timers
-  
-	function start() {
-	  if (!stopped) { seconds = (+time * 60); }
-  
-	  stopped = false;
-	  document.getElementById('start1').html('Stop');
-	  document.getElementById('down1').setAttribute('onclick', '');
-	  document.getElementById('up1').setAttribute('onclick', '');
-	  document.getElementById('start1').setAttribute('onclick', 'stop()');
-	  timer = setTimeout(countDown1, 1000);
-  
-	  function countDown() {
-		if (seconds > 0) { seconds--; }
-		if (seconds > 0 && !stopped) {
-		  timer = setTimeout(countDown, 1000);
-		}
-		if (!stopped && seconds % 60 >= 10) {
-		  document.getElementById('time1').html(
-			Math.floor(seconds / 60) + ':' + seconds % 60
-		  );
-		}
-		else if (seconds % 60 < 10) {
-		  if (seconds % 60 >= 0) {
-			if (seconds / 60 !== 0) {
-			  document.getElementById('time1').html(
-				Math.floor(seconds / 60) + ':0' + seconds % 60
-			  );
-			}
-		  }
-		}
-		else if (seconds / 60 === 0) {
-		  document.getElementById('time1').html('BREAK');
-		}
-	  } // counts down clock
-	} // starts clock
-  
-	function stop() {
-	  clearTimeout(timer);
-	  stopped = true;
-	  document.getElementById('start1').html('Start');
-	  document.getElementById('down1').setAttribute('onclick', 'down()');
-	  document.getElementById('up1').setAttribute('onclick', 'up()');
-	  document.getElementById('start1').setAttribute('onclick', 'start()');
-  
-	  if (seconds % 60 >= 10) {
-		document.getElementById('time1').html(
-		  Math.floor(seconds / 60) + ':' + seconds % 60
-		);
-	  }
-	  else {
-		document.getElementById('time1').html(
-		  Math.floor(seconds / 60) + ':0' + seconds % 60
-		);
-	  }
-	} // stops clock
-  
-	function reset() {
-	  if (stopped === false) {
-		clearTimeout(timer1);
-		document.getElementById('down1').setAttribute('onclick', 'down()');
-		document.getElementById('up1').setAttribute('onclick', 'up()');
-		document.getElementById('start1').setAttribute('onclick', 'start()');
-		document.getElementById('start1').html('Start');
-		stopped = true;
-	  }
-	  seconds = 1500;
-	  abrv1 = 2500;
-	  time1 = '25';
-  
-	  document.getElementById('time1').html(time1);
-	} // resets clock
+	document.getElementById(clockToUpdate).html(
+		minuteStr + ':' + tenthSecondStr + hundredthSecondStr
+	);
+}
+
+function runClock(clock) {
+	while (clockToRun) { setInterval( countDown, 1000 ); }
 }
 
 function toggleClocks() {
 	if (activeKing.dataset.name === 'blue') {
-		// stop clock2
-		// start clock1
+		clock2 = false;
+		clock1 = true;
+		clockToUpdate = 'clock1'
+		runClock(clock1);
 	}
 	else {
-		// stop clock1
-		// start clock2
+		clock1 = false;
+		clock2 = true;
+		clockToUpdate = 'clock2'
+		runClock(clock2);
 	}
 }
+
+///////////////////////////
+///////////////////////////
 
 function inCheck() {
 
@@ -781,6 +727,13 @@ function endOfGame() {
 	alert(activeKing.dataset.side + ' KING CHECKMATED!');
 	console.log(activeKing.dataset.side + ' KING CHECKMATED!');
 	console.log('END OF GAME');
+}
+
+function resign() {
+	activeSide.forEach(activePiece => {
+		activePiece.removeEventListener('click', wherePieceCanMove);
+	});
+	alert(activeKing.dataset.side + " resigns");
 }
 
 function pawnLit() {
