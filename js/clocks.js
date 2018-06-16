a2d42377da95867b1fda62d2de36cfba1b5bba27
byndasma <byndasma@gmail.com>
2018-06-16 13:26:55 -0700

    updated var names for clocks.js

--- js/clocks.js
+++ js/clocks.js
@@ -1,158 +1,146 @@
-$( document ).ready( function page() {
+var interval = null,
+    minuteNum = null,
+    colon = ':',
+    tenthSecondStr = '5',
+    tenthSecondNum = +tenthSecondStr,
+    hundredthSecondStr = '9',
+    hundredthSecondNum = +hundredthSecondStr,
+    mainTime = '',
+    focusTime = 10,
+    relaxTime = 10;
 
-  var interval = null,
-      minute_num = null,
-      colon = ':',
-      tenth_second_str = '5',
-      tenth_second_num = +tenth_second_str,
-      hundredth_second_str = '9',
-      hundredth_second_num = +hundredth_second_str,
-      main_time = '',
-      focus_time = 25,
-      relax_time = 8;
+function countDown() {
+  interval = setInterval( runClock,  1000 );
+}
 
+function runClock() { // runs clock down to 0
 
-  $( 'reset' ).addClass( 'noClick' );
+  minute_str = minuteNum.toString();
 
+  tenthSecondStr = tenthSecondNum.toString();
 
-  function countDown() {
-    interval = setInterval( runClock,  1000 );
+  hundredthSecondStr = hundredthSecondNum.toString();
+
+  mainTime = minute_str + colon + tenthSecondStr + hundredthSecondStr;
+
+  $( 'countDown' ).html( mainTime );
+
+  hundredthSecondNum -= 1;
+
+  if( hundredthSecondNum < 0 ) {
+    tenthSecondNum -= 1;
+    hundredthSecondNum = 9;
   }
-
-
-  function runClock() { // runs clock down to 0, then swaps times and repeats
-
-    minute_str = minute_num.toString();
-
-    tenth_second_str = tenth_second_num.toString();
-
-    hundredth_second_str = hundredth_second_num.toString();
-
-    main_time = minute_str + colon + tenth_second_str + hundredth_second_str;
-
-    $( 'countDown' ).html( main_time );
-
-    hundredth_second_num -= 1;
-
-    if( hundredth_second_num < 0 ) {
-      tenth_second_num -= 1;
-      hundredth_second_num = 9;
-    }
-    if( tenth_second_num < 0 ) {
-      minute_num -= 1;
-      tenth_second_num = 5;
-    }
-    if( minute_num < 0 ) {
-      transition();
-    }
+  if( tenthSecondNum < 0 ) {
+    minuteNum -= 1;
+    tenthSecondNum = 5;
   }
-
-  function transition() {
-
-    if ( $( 'clocks' ).hasClass( 'focusMode' ) ) { 
-
-      clearInterval( interval );
-      minute_num = relax_time;
-      $( 'clocks' ).removeClass( 'focusMode' );
-      $( 'focus, clock1' ).removeClass( 'active' );
-      $( 'relax' ).trigger( 'click' );
-      $( '#relax' ).trigger( 'play' );
-    }
-    else {
-
-      clearInterval( interval );
-      minute_num = focus_time;
-      $( 'clocks' ).removeClass( 'relaxMode' );
-      $( 'relax, clock2' ).removeClass( 'active' );
-      $( 'focus' ).trigger( 'click' );
-      $( '#focus' ).trigger( 'play' );
-    }
+  if( minuteNum < 0 ) {
+    transition();
   }
+}
+
+function transition() {
+
+  if ( $( 'clocks' ).hasClass( 'focusMode' ) ) { 
+
+    clearInterval( interval );
+    minuteNum = relaxTime;
+    $( 'clocks' ).removeClass( 'focusMode' );
+    $( 'focus, clock1' ).removeClass( 'active' );
+    $( 'relax' ).trigger( 'click' );
+    $( '#relax' ).trigger( 'play' );
+  }
+  else {
+
+    clearInterval( interval );
+    minuteNum = focusTime;
+    $( 'clocks' ).removeClass( 'relaxMode' );
+    $( 'relax, clock2' ).removeClass( 'active' );
+    $( 'focus' ).trigger( 'click' );
+    $( '#focus' ).trigger( 'play' );
+  }
+}
 
 
-  $( 'focusTime' ).html( focus_time );
+$( 'focusTime' ).html( focusTime );
 
 
-  $( 'relaxTime' ).html( relax_time );
+$( 'relaxTime' ).html( relaxTime );
 
 
-  $( 'focus' ).on( 'click', function runFocus() {
+$( 'focus' ).on( 'click', function runFocus() {
 
-      minute_num = focus_time - 1;
-      $( 'adjust1, adjust2, focus, relax' ).addClass( 'darken' );
-      $( 'reset' ).removeClass( 'noClick' ).addClass( 'lighten' );
-      $( 'countDown span' ).html( focus_time + ':00' );
-      $( 'focus, clock1' ).addClass( 'active' );
-      $( 'clocks' ).addClass( 'focusMode' );
-      $( 'reset' ).html( 'RESET' );
+    minuteNum = focusTime - 1;
+    $( 'adjust1, adjust2, focus, relax' ).addClass( 'darken' );
+    $( 'reset' ).removeClass( 'noClick' ).addClass( 'lighten' );
+    $( 'countDown span' ).html( focusTime + ':00' );
+    $( 'focus, clock1' ).addClass( 'active' );
+    $( 'clocks' ).addClass( 'focusMode' );
+    $( 'reset' ).html( 'RESET' );
 
-      countDown();
-  } );
-
-
-  $( 'relax' ).on( 'click', function runRelax() {
-
-      minute_num = relax_time - 1;
-      $( 'adjust1, adjust2, focus, relax' ).addClass( 'darken' );
-      $( 'reset' ).removeClass( 'noClick' ).addClass( 'lighten' );
-      $( 'countDown span' ).html( relax_time + ':00' );
-      $( 'relax, clock2' ).addClass( 'active' );
-      $( 'clocks' ).addClass( 'relaxMode' );
-      $( 'reset' ).html( 'RESET' );
-      countDown();
-  } );
+    countDown();
+} );
 
 
-  $( 'reset' ).on( 'click', function reset() {
-    $( 'clocks' ).removeClass( 'focusMode relaxMode' );
-    location.reload();
-  } );
+$( 'relax' ).on( 'click', function runRelax() {
+
+    minuteNum = relaxTime - 1;
+    $( 'adjust1, adjust2, focus, relax' ).addClass( 'darken' );
+    $( 'reset' ).removeClass( 'noClick' ).addClass( 'lighten' );
+    $( 'countDown span' ).html( relaxTime + ':00' );
+    $( 'relax, clock2' ).addClass( 'active' );
+    $( 'clocks' ).addClass( 'relaxMode' );
+    $( 'reset' ).html( 'RESET' );
+    countDown();
+} );
 
 
-  $( 'up1' ).on( 'click', function additionFocus() {
-
-    let plus_focus = focus_time + 1;
-
-    if( focus_time < 60 ) {
-      $( 'focusTime' ).html( plus_focus );
-      focus_time = plus_focus;
-    }
-  } );
+$( 'reset' ).on( 'click', function reset() {
+  $( 'clocks' ).removeClass( 'focusMode relaxMode' );
+  location.reload();
+} );
 
 
-  $( 'up2' ).on( 'click', function additionRelax () {
+$( 'up1' ).on( 'click', function additionFocus() {
 
-    let plus_relax = relax_time + 1;
+  let plus_focus = focusTime + 1;
 
-    if( relax_time < 60 ) {
-      $( 'relaxTime' ).html( plus_relax );
-      relax_time = plus_relax;
-    }
-  } );
-
-
-  $( 'down1' ).on( 'click', function subtractionFocus () {
-
-    let minus_focus = focus_time - 1;
-
-    if( minus_focus >= 1 ) {
-      $( 'focusTime' ).html( minus_focus );
-      focus_time = minus_focus;
-    }
-  } );
+  if( focusTime < 60 ) {
+    $( 'focusTime' ).html( plus_focus );
+    focusTime = plus_focus;
+  }
+} );
 
 
-  $( 'down2' ).on( 'click', function subtractionRelax () {
+$( 'up2' ).on( 'click', function additionRelax () {
 
-    let minus_relax = relax_time - 1;
+  let plus_relax = relaxTime + 1;
 
-    if ( minus_relax >= 1) {
-      $( 'relaxTime' ).html( minus_relax );
-      relax_time = minus_relax;
-    }
-  } );
+  if( relaxTime < 60 ) {
+    $( 'relaxTime' ).html( plus_relax );
+    relaxTime = plus_relax;
+  }
+} );
 
 
-} ); // closes document... page()
+$( 'down1' ).on( 'click', function subtractionFocus () {
 
- // STILL-TO-DO: color gradients for focus and relax
+  let minus_focus = focusTime - 1;
+
+  if( minus_focus >= 1 ) {
+    $( 'focusTime' ).html( minus_focus );
+    focusTime = minus_focus;
+  }
+} );
+
+
+$( 'down2' ).on( 'click', function subtractionRelax () {
+
+  let minus_relax = relaxTime - 1;
+
+  if ( minus_relax >= 1) {
+    $( 'relaxTime' ).html( minus_relax );
+    relaxTime = minus_relax;
+  }
+} );
