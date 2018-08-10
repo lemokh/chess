dc0ceded1ff3822b1f033266a1e17f0993ac28c4
byndasma <byndasma@gmail.com>
2018-08-10 14:04:51 -0400

    modulating codebase

--- index.js
+++ index.js
@@ -40,6 +40,46 @@ var blueNodes = board.querySelectorAll("[data-side='blue']"),
 		hundredths: 0 
 	};
 
+	function setTimer() { // unnecessary?
+		document.getElementById('start')
+				.addEventListener('click', getMinutes);
+	}
+	
+	function startClock() { runTimer = setInterval(countDown, 1000); };
+	
+	function countDown() {
+	
+		obj.hundredths -= 1;
+	  
+		if ( obj.hundredths < 0 ) {
+			obj.tenths -= 1;
+			obj.hundredths = 9;
+		}
+		if ( obj.tenths < 0 ) {
+			  obj.minutes -= 1;
+			  obj.tenths = 5;
+		}
+		if ( obj.minutes < 0 ) { return resign(); }
+	
+		clockToUpdate.innerHTML =  
+			obj.minutes + ':' + obj.tenths + obj.hundredths;
+	}
+	
+	function toggleClocks() {
+	
+		clearInterval(runTimer);
+		
+		if (activeSide[0].dataset.side === 'blue') {	
+			obj = blueTime;
+			clockToUpdate = clock1;
+		}
+		else { 
+			obj = orangeTime;
+			clockToUpdate = clock2;
+		}
+		startClock();
+	}
+
 ////////////////////////////////////////////////////////////
 
 function resign() {
@@ -63,12 +103,6 @@ function draw() {
 }
 
 ////////////////////////////////////////////////////////////
-
-function setTimer() { // unnecessary?
-	document.getElementById('start')
-			.addEventListener('click', getMinutes);
-}
-
 ////////////////////////////////////////////////////////////
 
 function lit() {
