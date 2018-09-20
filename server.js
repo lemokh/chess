d59577ccb1ccc72ca64a62aeae09ee2a3a36f85a
byndasma <byndasma@gmail.com>
2018-09-20 01:59:51 -0400

    server.js

--- server.js
+++ server.js
@@ -3,37 +3,49 @@
 var http = require('http'),
     fs = require('fs');
 
-function handle_incoming_request(req, res) {
-    console.log('ICOMING REQUEST: ' + req.method + ' ' + req.url);
-    res.writeHead(200, { 'Content-Type': 'application/json'});
-    res.end(JSON.stringify( { error: null } ) + '\n');
-}
-
-var s = http.createServer(handle_incoming_request);
-s.listen(8080);
-
-/*
-function process_request(req, res) {
-    var body = 'Thanks for calling!\n';
-    var content_length = body.length;
-    res.writeHead(200, {
-        'Content-Length': content_length,
-        'Content-Type': 'text-plain'
-    });
-    res.end(body);
-}
-
-var s = http.createServer(process_request);
-s.listen(8080);
-*/
-
-
 function load_album_list(callback) {
-    fs.readdir( 'albums', function(err, files) {
+    fs.readdir('albums', function(err, files) {
         if (err) {
             callback(err);
             return;
         }
-        callback(null, files);
+        var only_dirs = [];
+        (function interator(index) {    
+            if (index == files.length) {
+                callback(null, only_dirs);
+                return;
+            }
+            fs.stat(
+                "albums/" + files[index],
+                function(err, stats) {
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
+                    if (stats.isDirectory()) {
+                        only_dirs.push(files[index]);
+                    }
+                    iterator(index + 1);
+                }
+            );
+        })(0);
     });
 }
+
+function handle_incoming_request(req, res) {
+    console.log('INCOMING REQUEST: ' + req.method + ' ' + req.url);
+    load_album_list(function(err, albums) {
+        if (err) {
+            res.writeHead(503, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(err) + "\n");
+            return;
+        }
+        var out = { error: null, data: { albums: albums } };
+
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(out) + "\n");
+    });
+}
+
+var s = http.createServer(handle_incoming_request);
+s.listen(8080);
