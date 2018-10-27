c1f5c9213d2c880e4f43613d048f4c192e3856b2
byndasma <byndasma@gmail.com>
2018-10-27 00:11:47 -0400

    cancelGame() complete

--- main.js
+++ main.js
@@ -40,6 +40,8 @@ var board = document.getElementById('board'),
 	setBoard = blues.concat(oranges).map(piece => [piece.id, piece.src]),
 
 	elem = document.createElement('H1'),
+	spinner = document.createElement('H1'),
+
 	/////////////////////////////////////////////////////////////////////
 
 	userInput = 10,
@@ -1874,11 +1876,108 @@ function lit() {
 // 		case 45, 46, 69: return false;
 // 	}
 // }
+/////////////////////////////
+
+function cancelGame() {
+
+	document.getElementById('start').removeEventListener('click', cancelGame);
+	document.getElementById('start').addEventListener('click', getMinutes);
+	document.getElementById('start').innerHTML = 'SET TIMER';
+
+	elem.classList.add('gameLengths');
+	elem.parentNode.removeChild(elem);
+
+	spinner.style.display = 'none';
+	document.getElementById('time1').style.display = 'none';
+	document.getElementById('time2').style.display = 'none';
+
+	////////////////////////////////
+
+	document.getElementById('chooseGame').style.display = 'flex';
+	document.getElementById('offerGame').style.display = 'block';
+	document.getElementById('timeSet').style.display = 'block';
+}
+
+/////////////////////////////
+
+function getMinutes() {
+
+	timerSet = document.getElementById('timeSet').value;
+
+	elem.innerHTML = timerSet;
+	elem.classList.add('gameLengths');
+	document.getElementById('gameList').appendChild(elem);
+	
+	if (timerSet) {
+		if (timerSet > 0) {
+			if (timerSet < 1000) {
+				// MUST PREVENT '.' & 'e' FROM KEYDOWN
+				if (!timerSet.includes('.')) {
+					if (!timerSet.includes('e')) {
+						//////////////////////////////////////////////////////////////////////////
+						document.getElementById('start').removeEventListener('click', getMinutes);
+						document.getElementById('start').addEventListener('click', cancelGame);
+						document.getElementById('start').innerHTML = 'CANCEL GAME';
+
+						document.getElementById('chooseGame').style.display = 'none';
+						document.getElementById('offerGame').style.display = 'none';
+						document.getElementById('timeSet').style.display = 'none';
+						
+						spinner.style.display = 'flex';
+						spinner.innerHTML = 'AWAITING OPPONENT...';
+						document.querySelector('.modalContent').appendChild(spinner).classList.remove('gameLengths');
+						/////////////////////////////////////////////////////////////////////////////////////////////
+
+						userInput = +(timerSet);
+
+						clock1 = document.getElementById('time1');
+						clock1.innerHTML = userInput + ':00';
+
+						clock2 = document.getElementById('time2');
+						clock2.innerHTML = userInput + ':00';
+
+						blueTime = {
+							minutes: userInput,
+							tenths: 0,
+							hundredths: 0
+						};
+
+						orangeTime = {
+							minutes: userInput,
+							tenths: 0,
+							hundredths: 0
+						};
+
+						function showTimers(timer) {
+							timer.style.display = 'block';
+							timer.style.visibility = "visible";
+							timer.style.opacity = '1';
+							timer.style.transform = 'scale(1.0)';
+							timer.style.transition = 'visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s';
+						}
+
+						showTimers(document.getElementById('time1'));
+						showTimers(document.getElementById('time2'));
+
+						// once socket confirms that player2 accepts game
+						// document.getElementById('modal').style.display = 'none';
+						// document.getElementById('resign').classList.remove('noClick');
+						// lit();
+					}
+				}
+			}
+		}
+	}
+}
 
 /////////////////////////////
 
 window.onload = function() {
+	
+	document.getElementById('resign').classList.add('noClick');
 
+	document.getElementById('start').addEventListener('click', getMinutes);
+	
 	/*
 	var socket = io();
 	document.querySelector('button').addEventListener('click', function(e) {
@@ -1895,91 +1994,4 @@ window.onload = function() {
 	});
 	*/
 
-	document.getElementById('resign').classList.add('noClick');
-
-
-	function getMinutes() {
-		
-		document.getElementById('resign').classList.remove('noClick');
-
-		timerSet = document.getElementById('timeSet').value;
-
-		elem.innerHTML = timerSet;
-		elem.classList.add('gameLengths');
-		document.querySelector('#chooseGame').appendChild(elem);
-		
-        if (timerSet) {
-			if (timerSet > 0) {
-				if (timerSet < 1000) {
-					// MUST PREVENT THESE FROM KEYDOWN
-					if (!timerSet.includes('.')) {
-						if (!timerSet.includes('e')) {
-
-							document.getElementById('start').removeEventListener('click', getMinutes);
-
-							userInput = +(timerSet);
-
-							clock1 = document.getElementById('time1');
-							clock1.innerHTML = userInput + ':00';
-
-							clock2 = document.getElementById('time2');
-							clock2.innerHTML = userInput + ':00';
-
-							blueTime = {
-								minutes: userInput,
-								tenths: 0,
-								hundredths: 0
-							};
-
-							orangeTime = {
-								minutes: userInput,
-								tenths: 0,
-								hundredths: 0
-							};
-
-							// document.getElementById('acceptGame').style.display = "none";
-							document.getElementById('chooseGame').style.display = 'none';
-							document.getElementById('offerGame').style.display = 'none';
-							document.getElementById('timeSet').style.display = 'none';
-							document.getElementById('start').innerHTML = 'CANCEL GAME';
-							
-							elem.style.display = 'flex';
-							elem.innerHTML = 'AWAITING OPPONENT...';
-							document.querySelector('.modalContent').appendChild(elem).classList.remove('gameLengths');
-
-							function cancelGame() {
-								document.getElementById('start').removeEventListener('click', cancelGame);
-								document.getElementById('start').addEventListener('click', getMinutes);
-																
-								elem.classList.add('gameLengths');
-								document.querySelector('#chooseGame').appendChild(elem);
-
-								document.getElementById('start').innerHTML = 'SET TIMER';
-								document.getElementById('chooseGame').style.display = 'flex';
-								document.getElementById('offerGame').style.display = 'block';
-								document.getElementById('timeSet').style.display = 'block';
-								elem.style.display = 'none';
-							}
-
-							document.getElementById('start').addEventListener('click', cancelGame);
-
-							function showTimers(timer) {
-								timer.style.visibility = "visible";
-								timer.style.opacity = '1';
-								timer.style.transform = 'scale(1.0)';
-								timer.style.transition = 'visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s';
-							}
-
-							showTimers(document.getElementById('time1'));
-							showTimers(document.getElementById('time2'));
-
-							// lit();
-						}
-					}
-				}
-			}
-		}
-	}
-
-	document.getElementById('start').addEventListener('click', getMinutes);
 }
